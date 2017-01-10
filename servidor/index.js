578660099044366a46645bd21b0e1d6c96022d11
Alejandro Gómez <agp901@gmail.com>
2017-01-10 20:40:43 +0100

    Prueba ruta
    creacion de una ruta de prueba en el servidor local

--- servidor/index.js
+++ servidor/index.js
@@ -10,6 +10,11 @@ var server = require("http").Server(app);
 //cargando libreria socket   //enlazado al servidor
 var io = require("socket.io")(server);
 
+//creacion de ruta de prueba
+app.get("/hola-mundo", function (req, res) {
+	res.send("Hola mundo desde una ruta");
+});
+
 //creando el servidor con express
 server.listen(6677, function () {
 	console.log("aplicación funcionando en http://localhost:6677")
