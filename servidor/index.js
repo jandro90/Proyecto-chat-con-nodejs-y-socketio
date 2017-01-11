5579bc0bf7b67f0c33299b5d3904f4b77af4a9b8
Alejandro Gómez <agp901@gmail.com>
2017-01-11 09:59:25 +0100

    creando conexion al socket
    al mismo tiempo obtenmos la ip de los usuarios

--- servidor/index.js
+++ servidor/index.js
@@ -15,6 +15,14 @@ app.get("/hola-mundo", function (req, res) {
 	res.send("Hola mundo desde una ruta");
 });
 
+//abrimos conexion al socket
+io.on("connection", function (socket) {
+	console.log("El cliente con IP: " + socket.handshake.address + " se ha conectado")
+});
+
+
+
+
 //creando el servidor con express
 server.listen(6677, function () {
 	console.log("aplicación funcionando en http://localhost:6677")
